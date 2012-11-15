4cbc085b86c4aa2086a58b3655d95ab50a7e93a4
Ravindra <ravi@cloudmunch.com>
2012-11-15 10:49:27 +0530

    added tests

--- src/main/java/com/ravindrakaranam/Calc.java
+++ src/main/java/com/ravindrakaranam/Calc.java
@@ -6,7 +6,19 @@ public class Calc {
 	System.out.println ("Calc created");
 	}
 	
-    public static int addNumbers (int x, int y) {
+    public static double add (double x, double y) {
     return x + y;
     }
+    
+    public static double subtract (double x, double y) {
+        return x + y;
+        }
+    
+    public static double multiply (double x, double y) {
+        return x * y;
+        }
+    
+    public static double divide (double x, double y) {
+        return x * y;
+        }
 }
--- src/test/java/com/ravindrakaranam/CalcTest.java
+++ src/test/java/com/ravindrakaranam/CalcTest.java
@@ -29,17 +29,77 @@ public class CalcTest
 		c=new Calc();
 		try
 		{
-			assertEquals(15,c.addNumbers(10,5),0);
-			System.out.println("Test 1 completed successfully.");
+			assertEquals(15,c.add(10,5),0);
+			System.out.println("Test Add numbers completed successfully.");
 		}
 		catch (AssertionError e)
 		{
+			System.out.println("Test Add numbers failed.");
 			System.out.println(e.getMessage());
 			throw e;
 			
 		}
 	}
 
+	@Test
+	public void test2()
+	{
+		// Can be run when reader methods and default 
+		// constructor are complete.
+		c=new Calc();
+		try
+		{
+			assertEquals(40,c.subtract(50,10),0);
+			System.out.println("Test subtract numbers completed successfully.");
+		}
+		catch (AssertionError e)
+		{
+			System.out.println("Test subtract numbers failed.");
+			System.out.println(e.getMessage());
+			throw e;
+			
+		}
+	}
+	
+	@Test
+	public void test3()
+	{
+		// Can be run when reader methods and default 
+		// constructor are complete.
+		c=new Calc();
+		try
+		{
+			assertEquals(200,c.multiply(20,10),0);
+			System.out.println("Test multiply numbers completed successfully.");
+		}
+		catch (AssertionError e)
+		{
+			System.out.println("Test multiply numbers failed.");
+			System.out.println(e.getMessage());
+			throw e;
+			
+		}
+	}
+	
+	@Test
+	public void test4()
+	{
+		// Can be run when reader methods and default 
+		// constructor are complete.
+		c=new Calc();
+		try
+		{
+			assertEquals(30,c.divide(150,5),0);
+			System.out.println("Test divide numbers completed successfully.");
+		}
+		catch (AssertionError e)
+		{
+			System.out.println("Test divide numbers failed.");
+			System.out.println(e.getMessage());
+			throw e;
+			
+		}
+	}
 	
 	public static void main(String[] args) 
 	{
